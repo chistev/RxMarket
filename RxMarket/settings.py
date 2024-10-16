6ca253ceffd95713b83c56815104038e100dea25
chistev <stephenowabie@gmail.com>
2024-10-16 20:57:26 +0100

    added images to the categories

--- RxMarket/settings.py
+++ RxMarket/settings.py
@@ -104,7 +104,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Where your static files are located
+
+# Media files (user-uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'  # Directory to store uploaded media files
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
